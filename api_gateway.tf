5a0f0dfec1f669ae59a8cbe5a61206f0cf509725
Dilushi Pabasara <ntpgyrnanayakkara@gmail.com>
2024-01-22 15:29:00 +0530

    update file

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,4 +1,3 @@
-
 resource "aws_api_gateway_rest_api" "my_api" {
   name        = "MyAPI"
   description = "My API"
--- lambda.tf
+++ lambda.tf
@@ -1,53 +1,52 @@
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "MyLambdaFunction2"
+  handler       = "index.handler"
+  runtime       = "python3.8"
+  role          = aws_iam_role.lambda_exec.arn
 
-resource "aws_api_gateway_rest_api" "my_api" {
-  name        = "MyAPI"
-  description = "My API"
+  filename         = "${path.module}/lambda/lambda_function.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda/lambda_function.zip")
 }
 
-resource "aws_api_gateway_resource" "api_resource" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
-  path_part   = "hello"
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role2"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-resource "aws_api_gateway_method" "api_method" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.api_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
+# Additional policies for the Lambda role...
+resource "aws_iam_policy" "lambda_logging_policy" {
+  name        = "lambda_logging_policy2"
+  description = "IAM policy for logging from a lambda"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        Effect   = "Allow",
+        Resource = "arn:aws:logs:*:*:*",
+      },
+    ],
+  })
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.api_resource.id
-  http_method = aws_api_gateway_method.api_method.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.my_lambda.invoke_arn
-}
-
-resource "aws_lambda_permission" "api_lambda_permission" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.my_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/GET/hello"
-}
-
-resource "aws_api_gateway_deployment" "my_api_deployment" {
-  depends_on = [aws_api_gateway_integration.lambda_integration]
-
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "dev" # stage name
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-data "aws_region" "current" {}
-
-output "hello_invoke_url" {
-  value = "https://${aws_api_gateway_rest_api.my_api.id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${aws_api_gateway_deployment.my_api_deployment.stage_name}/hello"
+resource "aws_iam_role_policy_attachment" "lambda_logs_attachment" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_logging_policy.arn
 }
